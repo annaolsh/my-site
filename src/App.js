86fa1d57afbe1a5b75ab1c89c2215bef346ee996
github username <olshevskaia.anna@gmail.com>
2018-09-27 22:37:51 -0400

    reverted

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React, { Component } from 'react';
 import Header from './components/Header.js';
 import MainContainer from './containers/MainContainer.js';
+// import './App.css';
 import './App.css';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faStar } from '@fortawesome/free-solid-svg-icons';
-
-library.add(faStar);
 
 class App extends Component {
-
-
   render() {
     return (
       <div>
--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,24 +1,14 @@
-import React, { Component } from 'react';
-import skills from '../assets/json/skills.json';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import Skills from './Skills.js';
+import React from 'react';
 
-export default class AboutMe extends Component {
-    constructor(){
-        super()
-        this.state={
-          skills: skills
-        }
-    }
-
-    render() {
-        return (
-            <div id="about-me-component">
-                <h2>About me</h2>
-                <p>I am an allround web developer. I am a senior programmer with good knowledge of front-end techniques. I love structure and order and I also stand for quality. I love spending time on fixing little details and optimizing web apps. Also I like working in a team, you'll learn faster and much more. As the saying goes: 'two heads are better than one'.</p>
-                <Skills></Skills>
-            </div>
-        )
-    }
-    
+export default () => {
+  return (
+  <div id="about-me-component">
+    <h2>About me</h2>
+    <p>I am an allround web developer. I am a senior programmer with good knowledge of front-end techniques. I love structure and order and I also stand for quality. I love spending time on fixing little details and optimizing web apps. Also I like working in a team, you'll learn faster and much more. As the saying goes: 'two heads are better than one'.</p>
+    <h2>Skills</h2>
+    <div id="skillset" class="row">
+        Hello
+    </div>
+  </div>
+  )
 }
