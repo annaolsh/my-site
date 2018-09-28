9e81bbba29ad79aada8fdd13ece0f51b243d1a32
github username <olshevskaia.anna@gmail.com>
2018-09-27 21:50:02 -0400

    keep working on skillset

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -10,25 +10,64 @@ export default class Skills extends Component {
         }
     }
 
-    createSkillSet() {
-
+    _numberToStars(number) {
+        if (number === 5) {
+            return (
+                <span className="star">
+                    <FontAwesomeIcon icon="star" color="#6DB65B" size="sm"/>
+                    <FontAwesomeIcon icon="star" color="#6DB65B" size="sm"/>
+                    <FontAwesomeIcon icon="star" color="#6DB65B" size="sm"/>
+                    <FontAwesomeIcon icon="star" color="#6DB65B" size="sm"/>
+                    <FontAwesomeIcon icon="star" color="#6DB65B" size="sm"/>
+                </span>
+            )
+        } else if (number === 4) {
+            return (
+                <span className="star">
+                    <FontAwesomeIcon icon="star" color="#6DB65B" size="sm"/>
+                    <FontAwesomeIcon icon="star" color="#6DB65B" size="sm"/>
+                    <FontAwesomeIcon icon="star" color="#6DB65B" size="sm"/>
+                    <FontAwesomeIcon icon="star" color="#6DB65B" size="sm"/>
+                    <FontAwesomeIcon icon="star" color="#rrr" size="sm"/>
+                </span>
+            )
+        } else {
+            return (
+                <span className="star">
+                    <FontAwesomeIcon icon="star" color="#6DB65B" size="sm"/>
+                    <FontAwesomeIcon icon="star" color="#6DB65B" size="sm"/>
+                    <FontAwesomeIcon icon="star" color="#6DB65B" size="sm"/>
+                    <FontAwesomeIcon icon="star" color="#rrr" size="sm"/>
+                    <FontAwesomeIcon icon="star" color="#rrr" size="sm"/>
+                </span>
+            )
+        }
     }
 
     render() {
         return (
             <div id="skills-component">
                 <h2>Skills</h2>
-                <FontAwesomeIcon icon="star" color="#6DB65B"
-                size="sm"/>
                 <div id="skillset" className="row">
                     <div className="col-md-6">
                         <ul>
-                            {this.state.skills.map((skill, i) => {
-                                const skillName = Object.keys(skill);
+                            {this.state.skills.slice(0, 6).map((skill, i) => {
                                 return (
-                                    <li>{skillName}
-                                    
-                                    <span className="fa fa-star checked"></span>
+                                    <li>
+                                        <span className="skill-name">{skill.name}</span>
+                                        {this._numberToStars(skill.value)}
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                    </div>
+                    <div className="col-md-6">
+                        <ul>
+                            {this.state.skills.slice(6, -1).map((skill, i) => {
+                                return (
+                                    <li>
+                                        <span className="skill-name">{skill.name}</span>
+                                        {this._numberToStars(skill.value)}
                                     </li>
                                 )
                             })}
